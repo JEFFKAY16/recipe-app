3178a135514b71d172547d6e2ff62f7f4ae3ddb3
Jeff Kayombo <jeffkayombo16@gmail.com>
2022-05-25 19:32:03 +0200

    Implmenet recipe food table details

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root "public_recipes#index"
 
   resources :recipes, only: [:index, :show, :new, :create]
+
   resources :recipes do
+    resources :recipe_food, only: [:show, :new, :create]
     member do
       get :delete
     end
---
+++ test/controllers/recipe_food_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class RecipeFoodControllerTest < ActionDispatch::IntegrationTest
+  test "should get show" do
+    get recipe_food_show_url
+    assert_response :success
+  end
+end
