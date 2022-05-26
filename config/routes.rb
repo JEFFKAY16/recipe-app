2dab370265b13f4b9419a3407b4bc97c60f5bf9d
Jeff Kayombo <jeffkayombo16@gmail.com>
2022-05-26 18:17:44 +0200

    add delete functionality on recipe foods table

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :recipes, only: [:index, :show, :new, :create]
 
   resources :recipes do
-    resources :recipe_food, only: [:show, :new, :create, :destroy]
+    resources :recipe_food, only: [:show, :new, :create, :edit, :update, :destroy]
     member do
       get :delete
     end
