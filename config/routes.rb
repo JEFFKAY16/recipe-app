006af8d2cc445c450a160a42e9350d1a8cc3c83b
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-05-26 16:00:06 -0300

    Fix food branch merge issue

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "public_recipes#index"
 
   resources :recipes, only: [:index, :show]
-  resources :foods, only: [:index]
+  resources :foods, only: [:index, :new, :create, :destroy]
   resources :general_shopping_list, only: [:index]
   resources :public_recipes, only: [:index]
 end
