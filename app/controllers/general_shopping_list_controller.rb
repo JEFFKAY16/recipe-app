53e942f318552d8bbbd4050c3d024e68c3036b80
Jeff Kayombo <jeffkayombo16@gmail.com>
2022-05-26 20:44:20 +0200

    Generate shooping list functionality, works well

--- app/controllers/general_shopping_list_controller.rb
+++ app/controllers/general_shopping_list_controller.rb
@@ -1,3 +1,14 @@
 class GeneralShoppingListController < ApplicationController
-  def index; end
+  def index
+    @user = current_user
+    @foods = @user.foods
+    @recipes = Recipe.where(user_id: current_user)
+    @total_price = 0
+
+    @quantity = 0
+    
+    @foods.each do |food|
+      @quantity += RecipeFood.joins(:food).where(food_id: food.id).sum( 'quantity') * food.price
+    end
+  end
 end
