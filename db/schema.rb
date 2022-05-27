1b1956fd15a51596d6282c8639a70a847cbdf1c6
Jeff Kayombo <jeffkayombo16@gmail.com>
2022-05-27 17:13:23 +0200

    Making the recipes public

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_24_215928) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_26_193833) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,7 +39,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_24_215928) do
     t.float "preparation_time"
     t.float "cooking_time"
     t.text "description"
-    t.boolean "public"
+    t.boolean "public", default: false
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
